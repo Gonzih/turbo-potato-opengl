62219671f52ad9a4eb6ad1ff7f8b0d49dec1379f
Elton Law <eltonlaw296@gmail.com>
2020-10-12 22:33:54 -0400

    refactor tunnel code move out of generate_maze

--- src/map.hpp
+++ src/map.hpp
@@ -2,6 +2,7 @@
 #include <utility>
 
 #include "random.hpp"
+#include "logging.hpp"
 
 #define WALL_CHARACTER '#'
 #define EMPTY_SPACE_CHARACTER ' '
@@ -18,59 +19,17 @@ class Rect {
             return ((y1 - y0) / 2) + y0;
         }
 
-        void render(std::vector<std::vector<char> > &map) {
-            for (int x = x0; x < x1; x++) {
-                for (int y = y0; y < y1; y++) {
-                    map[x][y] = EMPTY_SPACE_CHARACTER;
-                }
-            }
-        }
-};
-
-class Map {
-    private:
-        int width;
-        int height;
-        std::vector<std::vector<char> > map;
-
-        Rect gen_rect(int size_w_limit, int size_h_limit) {
-            int size_w = rand_int(3, size_w_limit);
-            int size_h = rand_int(3, size_h_limit);
-            Rect rect;
-
-            // start of rect can be anywhere on screen
-            rect.x0 = rand_int(0, width-size_w);
-            rect.y0 = rand_int(0, height-size_h);
-            // add size to starting point
-            rect.x1 = rect.x0 + size_w;
-            rect.y1 = rect.y0 + size_h;
-
-            return rect;
-        }
-
-        void generate_maze() {
-            int nrect = rand_int(5, 15);
-            Rect rects[nrect];
-
-            int i;
-
-            for (i = 0; i < nrect; i++) {
-                auto rect = gen_rect(width/4, height/4);
-                rect.render(map);
-                rects[i] = rect;
-            }
-
-            for (i= 0; i < nrect - 1; i++) {
-                Rect rect0 = rects[i];
-                Rect rect1 = rects[i+1];
-
+        void add_tunnel_to_existing(std::vector<std::vector<char> > &map,
+                std::vector<Rect> &existing_rects) {
+            if (existing_rects.size() > 1) {
+                Rect rect = existing_rects.back();
                 Rect tunnel_x;
                 Rect tunnel_y;
 
-                int r0_x = rect0.center_x();
-                int r0_y = rect0.center_y();
-                int r1_x = rect1.center_x();
-                int r1_y = rect1.center_y();
+                int r0_x = center_x();
+                int r0_y = center_y();
+                int r1_x = rect.center_x();
+                int r1_y = rect.center_y();
 
                 if (r0_x < r1_x) {
                     tunnel_x.x0 = r0_x;
@@ -97,9 +56,58 @@ class Map {
             }
         }
 
+        void render(std::vector<std::vector<char> > &map) {
+            // renders the rectangle on the map
+            for (int x = x0; x < x1; x++) {
+                for (int y = y0; y < y1; y++) {
+                    map[x][y] = EMPTY_SPACE_CHARACTER;
+                }
+            }
+        }
+};
+
+class Map {
+    private:
+        int width;
+        int height;
+        std::vector<std::vector<char>> map;
+        int nrect = rand_int(5, 15);
+        std::vector<Rect> rects;
+
+        Rect gen_rect(int size_w_limit, int size_h_limit) {
+            int size_w = rand_int(3, size_w_limit);
+            int size_h = rand_int(3, size_h_limit);
+            Rect rect;
+
+            // start of rect can be anywhere on screen
+            rect.x0 = rand_int(0, width-size_w);
+            rect.y0 = rand_int(0, height-size_h);
+            // add size to starting point
+            rect.x1 = rect.x0 + size_w;
+            rect.y1 = rect.y0 + size_h;
+
+            return rect;
+        }
+
+        void generate_maze() {
+            logger->info("Maze number of rectangles={}", nrect);
+            std::vector<Rect> rects;
+
+            for (int i = 0; i < nrect; i++) {
+                auto rect = gen_rect(width/4, height/4);
+                // renders the rectangle on the map
+                rect.render(map);
+                // connects rectangle to existing rectangles
+                rect.add_tunnel_to_existing(map, rects);
+                rects.push_back(rect);
+            }
+        }
+
     public:
-        Map(int w, int h)
-            : width(w), height(h), map(std::vector<std::vector<char> >(w, std::vector<char>(h, WALL_CHARACTER)))
+        Map(int w, int h) :
+            width(w),
+            height(h),
+            map(std::vector<std::vector<char>>(w, std::vector<char>(h, WALL_CHARACTER)))
         { generate_maze(); }
 
         const int get_width() { return width; }
