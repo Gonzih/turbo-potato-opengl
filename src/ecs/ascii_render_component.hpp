c5c3a799dead7bb900b9078ac846e1c0f813b5f8
Max Gonzih <gonzih@gmail.com>
2020-10-21 16:14:56 -0400

    File per component

---
+++ src/ecs/ascii_render_component.hpp
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <memory>
+
+#include "ecs.hpp"
+
+namespace ecs
+{
+    namespace components
+    {
+        class AsciiRenderComponent : public Component
+        {
+        private:
+            std::wstring symbol;
+        public:
+            AsciiRenderComponent() : symbol { L"" } {  };
+            virtual ~AsciiRenderComponent() override {  };
+
+            void set_symbol(std::wstring s)
+            { symbol = s; }
+
+            const std::wstring get_symbol()
+            { return symbol; }
+        };
+    }
+};
---
+++ src/ecs/position_component.hpp
@@ -0,0 +1,52 @@
+#pragma once
+
+#include <memory>
+
+#include "../geometry.hpp"
+#include "ecs.hpp"
+#include "movement.hpp"
+
+namespace ecs
+{
+    namespace components
+    {
+        class PositionComponent : public Component
+        {
+        private:
+            Point pos;
+        public:
+            PositionComponent() : pos { 0, 0 } {};
+            virtual ~PositionComponent() override {  };
+
+            void move_to(Point new_pos)
+            {
+                pos = new_pos;
+            }
+
+            void move(MovementDirection direction)
+            {
+                switch(direction)
+                {
+                    case MovementDirection::Up:
+                        pos.y -= 1;
+                        break;
+                    case MovementDirection::Down:
+                        pos.y += 1;
+                        break;
+                    case MovementDirection::Left:
+                        pos.x -= 1;
+                        break;
+                    case MovementDirection::Right:
+                        pos.x += 1;
+                        break;
+                    case MovementDirection::None:
+                        break;
+                }
+            }
+
+            const Point get_pos() { return pos; };
+            const int get_x() { return pos.x; };
+            const int get_y() { return pos.y; };
+        };
+    }
+};
