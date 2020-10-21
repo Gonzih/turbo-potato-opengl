f5d093b00b7e52aecca4f421779d4203b3226c0b
Max Gonzih <gonzih@gmail.com>
2020-10-21 17:54:44 -0400

    who caares

--- src/game.hpp
+++ src/game.hpp
@@ -15,140 +15,141 @@
 using namespace ecs;
 using namespace ecs::components;
 
-class Game {
-    private:
-        int screen_w, screen_h;
-        Map map;
-        Window main_win;
-        System system;
-        std::shared_ptr<Entity> player;
+class Game
+{
+private:
+    int screen_w, screen_h;
+    Map map;
+    Window main_win;
+    System system;
+    std::shared_ptr<Entity> player;
 
-    public:
-        explicit Game(int screen_w, int screen_h) :
-            screen_w { screen_w },
-            screen_h { screen_h },
-            map { screen_w, screen_h },
-            main_win { screen_w, screen_h, 0, 0 },
-            system {  },
-            player { system.add_entity() }
-        { };
+public:
+    explicit Game(int screen_w, int screen_h) :
+        screen_w { screen_w },
+        screen_h { screen_h },
+        map { screen_w, screen_h },
+        main_win { screen_w, screen_h, 0, 0 },
+        system {  },
+        player { system.add_entity() }
+    { };
 
-        void init()
+    void init()
+    {
+        init_player();
+    }
+
+    void init_player()
+    {
+        auto pos =  map.get_random_empty_coords();
+        log::info("Initializing player at (x, y)", pos.x, pos.y);
+
+        player->add_component<PositionComponent>(pos);
+        player->add_component<AsciiRenderComponent>(PLAYER_CHARACTER);
+    }
+
+    void regen_map()
+    {
+        log::info("Regenerating map");
+
+        Map newmap { screen_w, screen_h };
+        map = newmap;
+
+        auto pos =  map.get_random_empty_coords();
+        log::info("Initializing player at (x, y)", pos.x, pos.y);
+
+        player->get_component<PositionComponent>()->move_to(pos);
+    }
+
+    void render()
+    {
+        system.draw();
+
+        main_win.erase();
+
+        auto player_pos = player->get_component<PositionComponent>()->get_pos();
+        auto light_map = map.generate_light_map(player_pos, LIGHT_RADIUS);
+        int c;
+
+        for (int i = 0; i < map.get_width(); ++i)
         {
-            init_player();
-        }
-
-        void init_player()
-        {
-            auto pos =  map.get_random_empty_coords();
-            log::info("Initializing player at (x, y)", pos.x, pos.y);
-
-            player->add_component<PositionComponent>(pos);
-            player->add_component<AsciiRenderComponent>(PLAYER_CHARACTER);
-        }
-
-        void regen_map()
-        {
-            log::info("Regenerating map");
-
-            Map newmap { screen_w, screen_h };
-            map = newmap;
-
-            auto pos =  map.get_random_empty_coords();
-            log::info("Initializing player at (x, y)", pos.x, pos.y);
-
-            player->get_component<PositionComponent>()->move_to(pos);
-        }
-
-        void render()
-        {
-            system.draw();
-
-            main_win.erase();
-
-            auto player_pos = player->get_component<PositionComponent>()->get_pos();
-            auto light_map = map.generate_light_map(player_pos, LIGHT_RADIUS);
-            int c;
-
-            for (int i = 0; i < map.get_width(); ++i)
+            for (int j = 0; j < map.get_height(); ++j)
             {
-                for (int j = 0; j < map.get_height(); ++j)
-                {
-                    c = map.at(i, j);
+                c = map.at(i, j);
 
-                    if (!light_map.visible(i, j)) {
-                        if (map.memoized(i, j)) {
-                            c |= A_DIM;
-                        } else {
-                            c = '.' | A_DIM;
-                        }
+                if (!light_map.visible(i, j)) {
+                    if (map.memoized(i, j)) {
+                        c |= A_DIM;
                     } else {
-                        map.memoize(i, j);
+                        c = '.' | A_DIM;
                     }
-
-                    main_win.render_char(c, i, j);
+                } else {
+                    map.memoize(i, j);
                 }
+
+                main_win.render_char(c, i, j);
             }
-
-            for (auto& entity : system.get_entities())
-            {
-                if (entity->has_component<AsciiRenderComponent>() && entity->has_component<AsciiRenderComponent>())
-                    main_win.print(
-                            entity->get_component<AsciiRenderComponent>()->get_symbol(),
-                            entity->get_component<PositionComponent>()->get_x(),
-                            entity->get_component<PositionComponent>()->get_y());
-
-            }
-
-
-            main_win.refresh();
         }
 
-        void loop()
+        for (auto& entity : system.get_entities())
         {
-            int c;
-            MovementDirection direction;
-            std::chrono::time_point<std::chrono::system_clock> then = std::chrono::system_clock::now();
-            std::chrono::time_point<std::chrono::system_clock> now = std::chrono::system_clock::now();
+            if (entity->has_component<AsciiRenderComponent>() && entity->has_component<AsciiRenderComponent>())
+                main_win.print(
+                        entity->get_component<AsciiRenderComponent>()->get_symbol(),
+                        entity->get_component<PositionComponent>()->get_x(),
+                        entity->get_component<PositionComponent>()->get_y());
 
-            for(;;) {
-                now = std::chrono::system_clock::now();
-                auto milliseconds = std::chrono::duration_cast<std::chrono::milliseconds>(now - then);
-                auto ms = milliseconds.count();
+        }
 
-                if (ms > 100) {
-                    system.update();
-                    then = now;
-                }
 
-                system.collect_garbage();
-                render();
-                c = getch();
-                switch(c) {
-                    case KEY_UP:
-                        direction = MovementDirection::Up;
-                        break;
-                    case KEY_DOWN:
-                        direction = MovementDirection::Down;
-                        break;
-                    case KEY_RIGHT:
-                        direction = MovementDirection::Right;
-                        break;
-                    case KEY_LEFT:
-                        direction = MovementDirection::Left;
-                        break;
-                    case ' ':
-                        regen_map();
-                        continue;
-                };
+        main_win.refresh();
+    }
 
-                if (direction == MovementDirection::None)
+    void loop()
+    {
+        int c;
+        MovementDirection direction;
+        std::chrono::time_point<std::chrono::system_clock> then = std::chrono::system_clock::now();
+        std::chrono::time_point<std::chrono::system_clock> now = std::chrono::system_clock::now();
+
+        for(;;) {
+            now = std::chrono::system_clock::now();
+            auto milliseconds = std::chrono::duration_cast<std::chrono::milliseconds>(now - then);
+            auto ms = milliseconds.count();
+
+            if (ms > 100) {
+                system.update();
+                then = now;
+            }
+
+            system.collect_garbage();
+            render();
+            c = getch();
+            switch(c) {
+                case KEY_UP:
+                    direction = MovementDirection::Up;
+                    break;
+                case KEY_DOWN:
+                    direction = MovementDirection::Down;
+                    break;
+                case KEY_RIGHT:
+                    direction = MovementDirection::Right;
+                    break;
+                case KEY_LEFT:
+                    direction = MovementDirection::Left;
+                    break;
+                case ' ':
+                    regen_map();
                     continue;
+            };
 
-                if (map.can_move(player->get_component<PositionComponent>()->get_pos(), direction)) {
-                    player->get_component<PositionComponent>()->move(direction);
-                    direction = MovementDirection::None;
-                }
+            if (direction == MovementDirection::None)
+                continue;
+
+            if (map.can_move(player->get_component<PositionComponent>()->get_pos(), direction)) {
+                player->get_component<PositionComponent>()->move(direction);
+                direction = MovementDirection::None;
             }
         }
+    }
 };
