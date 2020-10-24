4eb13189b30edb1b9c117f66f6d0bbeb55b17f26
Elton Law <eltonlaw296@gmail.com>
2020-10-24 05:44:17 -0400

    fix Deleted implicitly-declared copy assignment operator err

--- src/geometry.hpp
+++ src/geometry.hpp
@@ -15,14 +15,7 @@ class Point {
 
         Point operator-(Point p)  { return Point(x - p.x, y - p.x); }
         Point operator+(Point p)  { return Point(x + p.x, y + p.x); }
-        bool operator==(const Point& p)  { return (x == p.x) && (y == p.y); }
-        // taken from https://stackoverflow.com/questions/20590656/error-for-hash-function-of-pair-of-ints
-        size_t operator()(const Point &p) const{
-            uintmax_t hash = std::hash<int>{}(p.x);
-            hash <<= sizeof(uintmax_t) * 4;
-            hash ^= std::hash<int>{}(p.y);
-            return std::hash<uintmax_t>{}(hash);
-        }
+        bool operator==(const Point& p) const { return (x == p.x) && (y == p.y); }
 };
 
 class Rect {
@@ -37,3 +30,15 @@ class Rect {
 int center_x(Rect r) { return ((r.x1 - r.x0) / 2) + r.x0; }
 int center_y(Rect r) { return ((r.y1 - r.y0) / 2) + r.y0; }
 Point center(Rect r) { return Point(center_x(r), center_y(r)); }
+
+namespace std {
+  template<> struct hash<Point> {
+    // hash fn taken from https://stackoverflow.com/questions/20590656/error-for-hash-function-of-pair-of-ints
+    size_t operator()(Point const& p) const {
+        uintmax_t hash = std::hash<int>{}(p.x);
+        hash <<= sizeof(uintmax_t) * 4;
+        hash ^= std::hash<int>{}(p.y);
+        return std::hash<uintmax_t>{}(hash);
+    }
+  };
+}
